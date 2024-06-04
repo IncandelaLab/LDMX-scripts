132fcf6f89f15e0a83477f9e5687fc9c26dc6cce
Danyi Zhang <danyizhang@ucsb.edu>
2024-06-04 14:57:07 -0700

    Add group label in args

--- EcalVeto-3.0/ecalveto_reco.py
+++ EcalVeto-3.0/ecalveto_reco.py
@@ -1,9 +1,12 @@
 import argparse
+from ast import arg
+import os
 
 parser = argparse.ArgumentParser(description='')
-parser.add_argument('-i','--indir', action='store', dest='indir', help='Directory of input file, absolute path')
-parser.add_argument('-o','--outdir', action='store', dest='outdir', help='Directory of output files, absolute path')
+parser.add_argument('-i','--infile', action='store', dest='infile', help='Directory of input file, absolute path')
+parser.add_argument('-o','--outdir', action='store', dest='outdir', help='Directory of output file, absolute path')
 parser.add_argument('-m','--maxEvent', type=int, action='store', dest='maxEvent', help='Maximum number of events to process')
+parser.add_argument('-g','--groupl', action='store', dest='group_label', help='Sample label')
 args = parser.parse_args()
 
 from LDMX.Framework import ldmxcfg
@@ -13,9 +16,10 @@ p.maxTriesPerEvent = 10000
 
 p.maxEvents = args.maxEvent
 
-p.inputFiles = [args.indir]
+p.inputFiles = [args.infile]
 
-p.outputFiles = [args.outdir]
+outfile = os.path.join(args.outdir, args.group_label + '.root')
+p.outputFiles = [outfile]
 p.termLogLevel = 0
 
 # EcalVeto

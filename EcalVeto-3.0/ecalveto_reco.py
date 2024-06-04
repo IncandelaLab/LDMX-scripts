c0aa24c12ab3b3f07db80a89a4fb2540f0dbc5f2
Danyi Zhang <danyizhang@ucsb.edu>
2024-06-04 14:39:20 -0700

    Configurate indir, outdir, maxEvent

--- EcalVeto-3.0/ecalveto_reco.py
+++ EcalVeto-3.0/ecalveto_reco.py
@@ -1,13 +1,21 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('-i','--indir', action='store', dest='indir', help='Directory of input file, absolute path')
+parser.add_argument('-o','--outdir', action='store', dest='outdir', help='Directory of output files, absolute path')
+parser.add_argument('-m','--maxEvent', action='store', dest='maxEvent', help='Maximum number of events to process')
+args = parser.parse_args()
+
 from LDMX.Framework import ldmxcfg
 p = ldmxcfg.Process('SegmipBDTReco')
 
 p.maxTriesPerEvent = 10000
 
-p.maxEvents = 100
+p.maxEvents = args.maxEvent
 
-p.inputFiles = ['mc_v14-8gev-8.0GeV-1e-ecal_photonuclear_run100_t1714988693.root']
+p.inputFiles = [args.indir]
 
-p.outputFiles = [f'mc_v14-8gev-8.0GeV-1e-ecal_photonuclear_run100_t1714988693_segmipBDTReco.root']
+p.outputFiles = [args.outdir]
 p.termLogLevel = 0
 
 # EcalVeto

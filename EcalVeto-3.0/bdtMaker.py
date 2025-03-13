518aa3a820f835788791ad60cd7fa798e0108b74
Oscar L. <78984910+oscarplewis@users.noreply.github.com>
2025-03-13 10:57:31 -0700

    Update bdtMaker.py with bug fix, feature names, optional SHAP analysis, and package imports (#72)
    * Update bdtMaker.py to take multiple file inputs
    
    bdtMaker.py can now take absolute paths to directories as inputs for background and signal paths, in addition to permitting single file inputs. In the case of directories the script will add all ROOT files in the directory to the TChain. In the case of single files, the individual files will be added to the respective TChains instead.
    
    * Bug fix in bdtMaker.py
    
    The script would print out the 'failed trigger' message every time a signal event was loaded instead of every time a signal event failed the trigger and was passed over. Now the debug message is printed out only when the event has failed the trigger.
    
    * bdtMaker.py updates: feature_names, shap plots, package import updates
    
    Incorporated feature names for BDT training so that the feature importance and SHAP plots display what the features are.
    
    Included new optional functions to export training events to CSV files for further SHAP analysis, and to plot the SHAP values summary bar chart describing model performance on the test data.
    
    Eliminated unnecessary package imports for faster runtime.
    
    * Update bdtMaker.py for nondescript overlay pass name
    
    * Change image format EcalVeto-3.0/bdtMaker.py
    
    Co-authored-by: Tamas Vami <tamas.almos.vami@cern.ch>
    
    ---------
    
    Co-authored-by: Tamas Vami <tamas.almos.vami@cern.ch>

--- EcalVeto-3.0/bdtMaker.py
+++ EcalVeto-3.0/bdtMaker.py
@@ -1,16 +1,16 @@
-import os
-import sys
-import logging
+import os, sys, logging
 import glob
-import argparse
 import ROOT as r
 import numpy as np
 import pickle as pkl
 import xgboost as xgb
 import matplotlib as plt
-from array    import array
+import pandas as pd
 from optparse import OptionParser
+import shap
 r.gSystem.Load('libFramework.so')
+
+print('All packages loaded!')
 
 mpl_logger = logging.getLogger('matplotlib')
 mpl_logger.setLevel(logging.WARNING)
@@ -50,7 +50,11 @@ class sampleContainer:
             print(f'Assigning branch address to TriggerResult')
             self.trigger = addBranch(self.tree, 'TriggerResult', '2eTrigger_{}'.format('overlay'))
 
+
     def root2PyEvents(self):
+        """
+        Loads events in the TChain up to maxEvts, formatting them as a Numpy array.
+        """
         self.events =  []
         print(f'Loading {self.tree.GetEntries()} events')
         # TChain.GetEntries() should return all entries across all TTrees in the TChain
@@ -61,10 +65,12 @@ class sampleContainer:
             
             if len(self.events) >= self.maxEvts: # skips rest of for loop past maxEvts
                 break
-            
+
             if self.isSig: # skips signal events which failed the trigger
-                print(f'Event {event_count} : failed trigger; skipped')
-                if not self.trigger.passed(): continue
+                # print(f'nElectrons = {self.trigger.getAlgoVar(3)}') # prints nElectrons counted by trigger
+                if not self.trigger.passed(): 
+                    print(f'Event {event_count} : failed trigger; skipped')
+                    continue
             
             result = self.ecalVeto
 
@@ -125,14 +131,20 @@ class sampleContainer:
             ]
 
             self.events.append(evt)
+
             print(f'Event {event_count} : event data loaded successfully')
 
         new_idx=np.random.permutation(np.arange(np.shape(self.events)[0]))
         self.events = np.array(self.events)
+
         np.take(self.events, new_idx, axis=0, out=self.events)
         print("Final Event Shape" + str(np.shape(self.events)))
 
     def constructTrainAndTest(self):
+        """
+        Splits data in self.events into training and testing subsets.
+        """
+
         self.train_x = self.events[0:int(len(self.events)*self.trainFrac)]
         self.test_x = self.events[int(len(self.events)*self.trainFrac):]
 
@@ -140,18 +152,135 @@ class sampleContainer:
         self.test_y = np.zeros(len(self.test_x)) + (self.isSig == True)
 
 class mergedContainer:
+    """
+    Merges signal and background sampleContainers into one.
+    """
     def __init__(self, sigContainer,bkgContainer):
+        # defines names of the features for better labeling
+        self.feat_names = [
+                # Base variables
+                'nReadoutHits', # f0
+                'summedDet', # f1
+                'summedTightIso', # f2
+                'maxCellDep', # f3
+                'showerRMS', # f4
+                'xStd', # f5
+                'yStd', # f6
+                'avgLayerHit', # f7
+                'stdLayerHit', # f8
+                'deepestLayerHit', # f9
+                'ecalBackEnergy', # f10
+                # MIP Tracking variables
+                'nStraightTracks', # f11
+                'firstNearPhLayer', # f12
+                'nNearPhHits', # f13
+                'photonTerritoryHits', # f14
+                'epSep', # f15
+                'epDot', # f16
+                # Longitudinal segment variables
+                'energy_s1', # f17
+                'xMean_s1', # f18
+                'yMean_s1', # f19
+                'layerMean_s1', # f20
+                'energy_s2', # f21
+                'yMean_s2', # f22
+                # Electron RoC variables
+                'eContEnergy_x1_s1', # f23
+                'eContEnergy_x2_s1', # f24
+                'eContYMean_x1_s1', # f25
+                'eContEnergy_x1_s2', # f26
+                'eContEnergy_x2_s2', # f27
+                'eContYMean_x1_s2', # f28
+                # Photon RoC variables
+                'gContNHits_x1_s1', # f29
+                'gContYMean_x1_s1', # f30
+                'gContNHits_x1_s2', # f31
+                # Outside RoC variables
+                'oContEnergy_x1_s1', # f32
+                'oContEnergy_x2_s1', # f33
+                'oContEnergy_x3_s1', # f34
+                'oContNHits_x1_s1', # f35
+                'oContXMean_x1_s1', # f36
+                'oContYMean_x1_s1', # f37
+                'oContYMean_x2_s1', # f38
+                'oContYStd_x1_s1', # f39
+                'oContEnergy_x1_s2', # f40
+                'oContEnergy_x2_s2', # f41
+                'oContEnergy_x3_s2', # f42
+                'oContLayerMean_x1_s2', # f43
+                'oContLayerStd_x1_s2', # f44
+                'oContEnergy_x1_s3', # f45
+                'oContLayerMean_x1_s3', # f46
+            ]
+        
+        # merges train_x, train_y data
         self.train_x = np.vstack((sigContainer.train_x,bkgContainer.train_x))
         self.train_y = np.append(sigContainer.train_y,bkgContainer.train_y)
         
+        # corrects for any NaN values in the data
         self.train_x[np.isnan(self.train_x)] = 0.000
         self.train_y[np.isnan(self.train_y)] = 0.000
 
+        # merges test_x, test_y data
         self.test_x  = np.vstack((sigContainer.test_x,bkgContainer.test_x))
         self.test_y  = np.append(sigContainer.test_y,bkgContainer.test_y)
         
-        self.dtrain = xgb.DMatrix(self.train_x,self.train_y)
-        self.dtest  = xgb.DMatrix(self.test_x,self.test_y)
+        # formats data for training
+        self.dtrain = xgb.DMatrix(self.train_x,self.train_y,feature_names=self.feat_names)
+        self.dtest  = xgb.DMatrix(self.test_x,self.test_y,feature_names=self.feat_names)
+
+        # formats data for export as CSV
+        self.train_x_df = pd.DataFrame(self.train_x, columns=self.feat_names)
+        self.test_x_df = pd.DataFrame(self.test_x, columns=self.feat_names)
+
+    def saveEventsAsCsv(self, out_dir):
+        """
+        Saves training and testing data as CSV files to the BDT output directory.
+        """
+
+        self.train_x_df.to_csv(os.path.join(out_dir, 'model_train_events.csv'), index=False)
+        print( 'Exported training data at {}'.format(os.path.join(out_dir, 'model_train_events.csv')) )
+        self.test_x_df.to_csv(os.path.join(out_dir, 'model_test_events.csv'), index=False)
+        print( 'Exported training data at {}'.format(os.path.join(out_dir, 'model_test_events.csv')) )
+
+def calcShap(evt_container:mergedContainer, model, out_dir:str):
+    """
+    Uses test data and model to calculate SHAP values for the BDT. 
+    Saves SHAP values as a npy file, and returns a numpy array containing
+    the values.
+    """
+    x_frame = evt_container.test_x_df
+
+    # picks n rows at random from the dataframe to initalize shap.TreeExplainer container
+    sample = x_frame.sample(n=100, random_state=42) # leaving random_state unset will make it truly random; enter value for repeatability
+    
+    explainer = shap.TreeExplainer(model, sample)
+    print(f'shap.TreeExplainer object created successfully')
+
+    print("Calculating shap values")
+    out_path = os.path.join(out_dir, 'model_shapValues.npy')
+
+    # calculates shap values and returns a numpy array, saving it to out_dir
+    # for large datasets, this should be a rather time consuming process
+    # 2 hrs for ~ 1M events
+    shap_values = explainer.shap_values(x_frame)
+    np.save(out_path, shap_values)
+
+    print(f'Shap values calculated!\nSaved at : {out_path}')
+
+    return shap_values
+
+def plotShap(evt_container:mergedContainer, shap_values, out_dir:str):
+    data = evt_container.test_x_df
+    num_features = len(data.columns)
+
+    shap_container = shap.Explanation(shap_values, data=data, feature_names=data.columns)
+
+    bar_mean_outpath = os.path.join(out_dir, 'model_bar_shapMeanAbs.png')
+    shap.plots.bar(shap_container, max_display=num_features, show=False)
+    plt.pyplot.savefig(bar_mean_outpath, bbox_inches='tight', dpi=500)
+    plt.pyplot.close()
+    print(f'SHAP summary bar plot with {num_features} features saved at {bar_mean_outpath}')
 
 if __name__ == "__main__":
     
@@ -166,6 +295,8 @@ if __name__ == "__main__":
     parser.add_option('-b', dest='bkg_path', help='Absolute path to background file(s)')
     parser.add_option('-s', dest='sig_path', help='Absolute path to signal file(s)')
     parser.add_option('-o', dest='out_name',  default='bdt_test', help='Output Pickle Name (excluding file extension)')
+    parser.add_option('--csv', action='store_true', dest='csv_bool', help='Whether or not to export the training and testing event features as a CSV file')
+    parser.add_option('--shap', action='store_true', dest='shap_bool', help='Whether or not to plot the SHAP values summary for the models performance on the test data')
     (options, args) = parser.parse_args()
 
     # pulls all files from the indicated input directories
@@ -184,21 +315,25 @@ if __name__ == "__main__":
         print(f'os.path cannot read sig_path as either a file or directory')
         sys.exit(f'Problem reading sig_path\nsig_path = {options.sig_path}\nPlease check inputted parameters!')
 
+
     # Seed numpy's randomness
     np.random.seed(options.seed)
    
     # Get BDT num
     bdt_num=0
+    outpath = options.out_name + '_'
     Check=True
     while Check:
-        if not os.path.exists(options.out_name+'_'+str(bdt_num)):
+        if not os.path.exists(outpath+str(bdt_num)):
             try:
-                os.makedirs(options.out_name+'_'+str(bdt_num))
+                os.makedirs(outpath+str(bdt_num))
                 Check=False
             except:
                Check=True
         else:
             bdt_num+=1
+    
+    outpath = outpath + str(bdt_num)
 
     # Print run info
     print( 'Random seed is = {}'.format(options.seed)             )
@@ -220,7 +355,12 @@ if __name__ == "__main__":
     bkgContainer.constructTrainAndTest()
 
     # Merge
+    print( 'Merging signal and background containers' )
     eventContainer = mergedContainer(sigContainer,bkgContainer)
+    
+    if options.csv_bool:
+        print( 'Exporting training and testing events as CSV file' )
+        eventContainer.saveEventsAsCsv(outpath)
 
     params = {
                'objective': 'binary:logistic',
@@ -243,15 +383,30 @@ if __name__ == "__main__":
     gbm = xgb.train(params, eventContainer.dtrain, num_boost_round = options.tree_number, evals = evallist, early_stopping_rounds = 10)
     
     # Store BDT
-    output = open(options.out_name+'_'+str(bdt_num)+'/' + \
-            options.out_name+'_'+str(bdt_num)+'_weights.pkl', 'wb')
+    output = open(outpath+'/' + \
+            outpath+'_weights.pkl', 'wb') # output file name
     pkl.dump(gbm, output)
 
+    # Calculate and plot shap values
+    if options.shap_bool:
+        print( 'Calculating SHAP values...' )
+        shap_values = calcShap(eventContainer, gbm, outpath)
+        print( 'Plotting SHAP summary...' )
+        plotShap(eventContainer, shap_values, outpath)
+
+
     # Plot feature importances
+    figure_path = outpath+'_fimportance.png'
     xgb.plot_importance(gbm)
-    plt.pyplot.savefig(options.out_name+'_'+str(bdt_num)+"/" + \
-            options.out_name+'_'+str(bdt_num)+'_fimportance.png', # png file name
+    plt.pyplot.savefig(outpath+"/" + \
+            figure_path, # png file name
             dpi=500, bbox_inches='tight', pad_inches=0.5) # png parameters
     
-    # Closing statment
-    print("Files saved in: ", options.out_name+'_'+str(bdt_num))
+
+    print(f'Feature importances saved under {figure_path}')
+
+    # Closing statement
+    print("All files saved in: ", outpath)
+
+if __name__ != "__main__":
+    print(f'Imported {__file__}!')

68d67ce3e382f32dc71c1d340337dce913e3658e
Tamas Vami <tamas.almos.vami@cern.ch>
2026-05-11 11:33:29 -0700

    Refactor electron gun configuration for consistency

--- EnergyResStudies/electron_gun_config.py
+++ EnergyResStudies/electron_gun_config.py
@@ -8,37 +8,32 @@ from LDMX.Framework import ldmxcfg
 
 # set a 'pass name'
 passName="sim"
-p=ldmxcfg.Process(passName)
+p = ldmxcfg.Process(passName)
 
 # Set run parameters.
-p.maxEvents = 10000
+p.max_events = 10000
 p.run = 1
-p.maxTriesPerEvent = 1
+p.max_tries_per_event = 1
 
 #import all processors
 from LDMX.SimCore import generators
 from LDMX.SimCore import simulator
-from LDMX.Biasing import filters
-
-from LDMX.Detectors.makePath import *
-from LDMX.SimCore import simcfg
 
 # Instantiate the simulator.
-sim = simulator.simulator("mySim")
+sim = simulator.Simulator(instance_name = "mySim")
 
 # Set the path to the detector to use (pulled from job config)
-detector='ldmx-det-v14-8gev'
-sim.setDetector( detector, True )
-sim.scoringPlanes = makeScoringPlanesPath(detector)
-sim.beamSpotSmear = [20., 80., 0]
+detector='ldmx-det-v15-8gev'
+sim.set_detector(detector, include_scoring_planes_minimal=True)
 
 
 # Setup the multi-particle gun
-mpgGen = generators.multi( "mgpGen" )                                                                           
-mpgGen.vertex = [ 0., 0., 200. ] # mm                                                                                                                              
-mpgGen.nParticles = 1
-mpgGen.pdgID = 11
-mpgGen.enablePoisson = False #True                                                                                     
+mpgGen = generators.Multi(instance_name = "mgpGen")
+mpgGen.vertex = [ 0., 0., 200. ] # mm
+mpgGen.n_particles = 1
+mpgGen.pdg_id = 11
+mpgGen.enable_poisson = False
+mpgGen.beam_spot_smear = [20., 80., 0.]
 
 # import math
 # import numpy as np
@@ -49,15 +44,15 @@ mpgGen.enablePoisson = False #True
 # pz= beamEnergyMeV*math.cos(theta)
 px = 0.
 py = 0.
-pz= 3000.
+pz = 3000.
 mpgGen.momentum = [ px, py, pz ]
 
 # Set the multiparticle gun as generator
 sim.generators = [ mpgGen ]
 
 #Ecal and Hcal geometry stuff
-from LDMX.Ecal import EcalGeometry
-from LDMX.Hcal import HcalGeometry
+import LDMX.Ecal.ecal_geometry
+import LDMX.Hcal.hcal_geometry
 import LDMX.Ecal.ecal_hardcoded_conditions
 
 # ecal digi chain
@@ -65,19 +60,18 @@ from LDMX.Ecal import digi as eDigi
 from LDMX.DQM import dqm
 
 
-ecalDigi   =eDigi.EcalDigiProducer('EcalDigis', si_thickness = 0.4)
-ecalReco   =eDigi.EcalRecProducer('ecalRecon')
+ecalDigi   = eDigi.EcalDigiProducer()
+ecalReco   = eDigi.EcalRecProducer()
 ecalDigiVerDQM = dqm.EcalDigiVerify()
 
-# default is 2 (WARNING); but then logFrequency is ignored. level 1 = INFO.
-p.logger.termLevel = 1 
-p.logFrequency = 10
+# default is 2 (WARNING); but then log_frequency is ignored. level 1 = INFO.
+p.logger.term_level = 1
+p.log_frequency = 10
 p.sequence=[ sim, ecalDigi, ecalReco, ecalDigiVerDQM]
 
 p.keep = [ "drop MagnetScoringPlaneHits", "drop TrackerScoringPlaneHits", "drop HcalScoringPlaneHits"]
 
-p.outputFiles=["simoutput.root"]
-p.histogramFile = f'hist.root'
+p.output_files = ["simoutput.root"]
+p.histogram_file = 'hist.root'
 
-print("Simulation configured to produce output files:", p.outputFiles, "and histogram file:", p.histogramFile)
-    
+print("Simulation configured to produce output files:", p.output_files, "and histogram file:", p.histogram_file)
